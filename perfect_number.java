fc60ddc89956b34ee31764cac72f2515905b656c
Umamaheswari <165144866+umamaheswari310@users.noreply.github.com>
2025-07-13 19:07:58 +0530

    Add files via upload

---
+++ perfect_number.java
@@ -0,0 +1,15 @@
+class Perfect{
+    public static void main(String[]args)
+    {
+       int n=48, sum=0;
+        for(int i=1;i<n;i++)
+        {
+         if (n%i==0) 
+         sum=sum+i;
+        }
+         if (sum==n)
+            System.out.println(n + " is a perfect number");
+         else
+            System.out.println(n + " is not a perfect number");
+    }
+}
