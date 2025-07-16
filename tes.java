e8b5fd45160a283d337a1f692ae1be02790d5ea7
Umamaheswari <165144866+umamaheswari310@users.noreply.github.com>
2025-07-16 20:04:51 +0530

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,8 @@
+class Test{
+int age;
+public static void main(String[]args){
+Test t1=new Test();
+System.out.println(t1.age);
+}
+}
+//Note:when you declare the variable inside the class & not assigned any value then automatically java inserts default values for non static variables only/ int,short,byte,long-0,double,float-0.0,boolean-false,String-null, char-unicode will generate
